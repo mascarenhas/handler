fb4f1168e4e87b2f82ecb84ae355503b80311381
Fabio Mascarenhas <mascarenhas@gmail.com>
2016-06-16 12:30:05 -0700

    add traceback to exception example

--- contrib/exception.lua
+++ contrib/exception.lua
@@ -1,4 +1,6 @@
 local handler = require "handler"
+local ok, coroutine = pcall(require, "taggedcoro")
+if not ok then coroutine = require "coroutine" end
 
 local ex = {}
 
@@ -7,14 +9,30 @@ function ex.trycatch(tblk, cblk)
   h["return"] = function (...)
     return ...
   end
-  function h.exception_throw(k, e)
-    return cblk(e, k)
+  function h.exception_throw(k, co, e)
+    local traceback = function (msg)
+      return ex.traceback(co, msg)
+    end
+    return cblk(e, traceback, k)
   end
   return handler.with(h, tblk)
 end
 
 function ex.throw(e)
-  return handler.op("exception_throw", e)
+  return handler.op("exception_throw", coroutine.running(), e)
+end
+
+function ex.traceback(co, msg)
+  local tb = { msg, "stack traceback:" }
+  while co do
+    tb[#tb+1] = debug.traceback(co):gsub("^stack traceback:\n", "")
+    if coroutine.caller then
+      co = coroutine.caller(co)
+    else
+      co = nil
+    end
+  end
+  return table.concat(tb, "\n")
 end
 
 return ex
